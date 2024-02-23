47d5ba52e83f46bba516cc38c9248d267ae52ef6
Mine Rala <mine-rala@hotmail.com>
2024-02-23 14:00:18 +0300

    Initial Commit

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Mine Rala on 23.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
