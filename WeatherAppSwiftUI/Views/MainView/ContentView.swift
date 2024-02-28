cc8303d783800111aa7d3b2b9bfff365b2ce876c
Hakan Uğraş <hakanugras422@gmail.com>
2024-02-28 09:30:36 +0300

    Added HomeView, SubView scroll effect

--- WeatherAppSwiftUI/Views/MainView/ContentView.swift
+++ WeatherAppSwiftUI/Views/MainView/ContentView.swift
@@ -9,40 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            ScrollView {
-                VStack(spacing: 16) {
-                    SubView()
-                    HourlyListView()
-                        .cornerRadius(10)
-                    DailyListView()
-                        .frame(height: 10 * 50)
-                        .cornerRadius(10)
-                    DataListView()
-                }
-                .frame(width: UIScreen.main.bounds.width - 40)
-            }
-            .tabItem {
-                Image(systemName: "1.circle")
-            }
-            .scrollIndicators(.hidden)
-            .background(
-                Image("hv")
-                    .resizable()
-                    .scaledToFill()
-                    .edgesIgnoringSafeArea(.all)
-            )
-            
-            AddView()
-                .background(.blue.opacity(0.5))
-                .tabItem {
-                    Image(systemName: "2.circle")
-                }
-        }
-        .onAppear() {
-                UITabBar.appearance().backgroundColor = .lightGray
-            }
-        .accentColor(.blue)
+        HomeView()
     }
 }
 
